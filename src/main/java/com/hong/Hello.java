29b0e0b5e3ae1618ffa0ea45145ef769db689b31
Hong <bettc123@gmail.com>
2021-07-10 16:09:40 +0800

    java and kotlin variable & date type

--- src/main/java/com/hong/Hello.java
+++ src/main/java/com/hong/Hello.java
@@ -6,5 +6,16 @@ public class Hello{
 //        new Person().Hello();  //呼叫Person類別裡的Hello功能
         Person p = new Person();
         p.Hello();
+//        "數值"基本資料型態:int,short,long,float,double,byte
+        int age = 19;
+        Integer age2 = 19;
+        age2.byteValue();
+//        "字元"資料型態:char(字元),String(字串)
+        char c = 'A';   //char c = '我';
+        float weight = 66.5f; //java 浮點數預設使用64位元的double(精確度高?),加上"f"可用較窄的32位元放入
+//        真假型態:boolean
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
         }
--- src/main/kotlin/com/hong/Hello.kt
+++ src/main/kotlin/com/hong/Hello.kt
@@ -6,6 +6,13 @@ fun main() {
 //儲存物件可用val、var來定義，「val 代表定義完後不能再改變他的值」
     val h = Human() //用val建構Human物件
     h.hello()
+//    一開始定義變數時，要用val(不可變動)或var(未來可變動)
+//    val age = 19; //可不指定資料型態。如要指定 val age(名稱) : Int(型態) =  19(值);
+    var age = 19;
+    age = 20;
+    var weight = 66.5; //預設是double型態，加"f"會轉變成float型態
+    var name : String; //只定義未給值，不加String系統不知道name是什麼 //直接給值就是var name = "Hong";
+    name = "Hong";
 }
 
 //先設計類別，再設計方法
